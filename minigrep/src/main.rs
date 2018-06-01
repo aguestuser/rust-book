25fbad1a0fcade0dfcf6e1bc05a71f701dab679e
aguestuser <aguestuser@riseup.net>
2018-06-01 16:01:52 -0400

    redirect error messages to `stderr`

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,12 +8,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let cfg = Config::new(&args).unwrap_or_else(|err| {
-        println!("Could not parse arguments: {}", err);
+        eprintln!("Could not parse arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(cfg) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
