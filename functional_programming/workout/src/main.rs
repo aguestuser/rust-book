52190ef630bf41e4c233c48def9df49b425ed6ac
aguestuser <aguestuser@riseup.net>
2018-06-02 14:24:52 -0400

    extend cacher to cache generic lambdas on generic types
    (provided types implement Copy/Eq/Hash)

--- functional_programming/workout/src/main.rs
+++ functional_programming/workout/src/main.rs
@@ -1,4 +1,6 @@
+use std::cmp::Eq;
 use std::collections::HashMap;
+use std::hash::Hash;
 use std::thread;
 use std::time::Duration;
 
@@ -28,25 +30,29 @@ fn generate_workout(intensity: u32, rand_num: u32) {
     }
 }
 
-struct Cacher<T>
+struct Cacher<F, A, B>
 where
-    T: Fn(u32) -> u32,
+    F: Fn(A) -> B,
+    A: Hash + Eq + Copy,
+    B: Copy,
 {
-    calculation: T,
-    values: HashMap<u32, u32>,
+    calculation: F,
+    values: HashMap<A, B>,
 }
 
-impl<T> Cacher<T>
+impl<F, A, B> Cacher<F, A, B>
 where
-    T: Fn(u32) -> u32,
+    F: Fn(A) -> B,
+    A: Hash + Eq + Copy,
+    B: Copy,
 {
-    fn new(calculation: T) -> Cacher<T> {
+    fn new(calculation: F) -> Cacher<F, A, B> {
         Cacher {
             calculation,
             values: HashMap::new(),
         }
     }
-    fn value(&mut self, arg: u32) -> u32 {
+    fn value(&mut self, arg: A) -> B {
         let val = self.values.entry(arg).or_insert((self.calculation)(arg));
         *val
     }
@@ -60,10 +66,27 @@ fn main() {
 }
 
 #[test]
-fn call_with_different_values() {
+fn cache_int_values() {
     let mut c = Cacher::new(|a| a);
 
     assert_eq!(c.value(1), 1);
     assert_eq!(c.value(2), 2);
     assert_eq!(c.value(1), 1);
+}
+
+#[test]
+fn cache_string_values() {
+    let mut c = Cacher::new(|a| a);
+
+    assert_eq!(c.value("foo"), "foo");
+    assert_eq!(c.value("bar"), "bar");
+}
+
+#[test]
+fn cache_string_slice_lenghts() {
+    let mut c = Cacher::new(|a: &str| a.len());
+
+    assert_eq!(c.value(&"foo"), 3);
+    assert_eq!(c.value(&"foos"), 4);
+    assert_eq!(c.value(&"foo"), 3);
 }
